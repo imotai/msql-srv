ebb6851f8fd2871a8aaacb74c83739b2bd2c4a03
imotai <codego.me@gmail.com>
2022-03-23 14:46:40 +0800

    fix: use mut self for authenticate_with_db

--- src/lib.rs
+++ src/lib.rs
@@ -216,10 +216,9 @@ pub trait MysqlShim<W: Write> {
         true
     }
 
-
     /// authenticate method for the specified plugin
     fn authenticate_with_db(
-        &self,
+        &mut self,
         _auth_plugin: &str,
         _username: &[u8],
         _salt: &[u8],
@@ -327,7 +326,7 @@ pub trait AsyncMysqlShim<W: Write + Send> {
 
     /// authenticate method for the specified plugin
     async fn authenticate_with_db(
-        &self,
+        &mut self,
         _auth_plugin: &str,
         _username: &[u8],
         _salt: &[u8],
