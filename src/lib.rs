315a1b81d393ca3b7cac683fa734238f7cba3d5c
imotai <codego.me@gmail.com>
2022-03-24 15:29:18 +0800

    Feat/remove select@ (#5)
    * fix: out put transactionlevel
    
    * fix: remove select proxy function

--- src/lib.rs
+++ src/lib.rs
@@ -699,50 +699,7 @@ impl<B: MysqlShim<W>, R: Read, W: Write> MysqlIntermediary<B, R, W> {
 
             match cmd {
                 Command::Query(q) => {
-                    if q.starts_with(b"SELECT @@") || q.starts_with(b"select @@") {
-                        let w = QueryResultWriter::new(
-                            &mut self.writer,
-                            false,
-                            self.client_capabilities,
-                        );
-                        let var = &q[b"SELECT @@".len()..];
-                        let var_with_at = &q[b"SELECT ".len()..];
-                        match var {
-                            b"max_allowed_packet" => {
-                                let cols = &[Column {
-                                    table: String::new(),
-                                    column: String::from_utf8_lossy(var_with_at).to_string(),
-                                    coltype: myc::constants::ColumnType::MYSQL_TYPE_LONG,
-                                    colflags: myc::constants::ColumnFlags::UNSIGNED_FLAG,
-                                }];
-                                let mut w = w.start(cols)?;
-                                w.write_row(iter::once(67108864u32))?;
-                                w.finish()?;
-                            }
-                            b"transaction_isolation" => {
-                                let cols = &[Column {
-                                    table: String::new(),
-                                    column: String::from_utf8_lossy(var_with_at).to_string(),
-                                    coltype: myc::constants::ColumnType::MYSQL_TYPE_STRING,
-                                    colflags: myc::constants::ColumnFlags::empty(),
-                                }];
-                                let mut w = w.start(cols)?;
-                                w.write_row(iter::once("REPEATABLE-READ"))?;
-                                w.finish()?;
-                            }
-                            _ => {
-                                let cols = &[Column {
-                                    table: String::new(),
-                                    column: String::from_utf8_lossy(var_with_at).to_string(),
-                                    coltype: myc::constants::ColumnType::MYSQL_TYPE_LONG,
-                                    colflags: myc::constants::ColumnFlags::UNSIGNED_FLAG,
-                                }];
-                                let mut w = w.start(cols)?;
-                                w.write_row(iter::once(0))?;
-                                w.finish()?;
-                            }
-                        }
-                    } else if !self.process_use_statement_on_query
+                    if !self.process_use_statement_on_query
                         && (q.starts_with(b"USE ") || q.starts_with(b"use "))
                     {
                         let w = InitWriter {
